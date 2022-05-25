d68ae8ba966c00bf46d21f72cf27523de3df087b
mitanvir05 <tanvir.cse05@gmail.com>
2022-05-25 20:11:12 +0600

    5 question added

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -2,8 +2,93 @@ import React from "react";
 
 const Blogs = () => {
   return (
-    <div>
-      <h1>This is a blog</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="card-title">
+            How will you improve the performance of a React Application?
+          </h2>
+          <p>
+            1. Using Immutable Data Structures <br /> 2. Function/Stateless
+            Components and React.PureComponent
+            <br />
+            3.Dependency optimization
+            <br />
+            4. Avoid Inline Function Definition in the Render Function.
+          </p>
+        </div>
+      </div>
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="card-title">
+            {" "}
+            What are the different ways to manage a state in a React
+            application?
+          </h2>
+          <p>
+            1 . Communication State <br />
+            2. Data State <br />
+            3. Control State <br />
+            4. Session State
+            <br />
+            5. Location State
+          </p>
+        </div>
+      </div>
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="card-title">How does prototypical inheritance work?</h2>
+          <p>
+            Every object with its methods and properties contains an internal
+            and hidden property known as Prototype. The Prototypal Inheritance
+            is a feature in javascript used to add methods and properties in
+            objects. It is a method by which an object can inherit the
+            properties and methods of another object. Traditionally, in order to
+            get and set the Prototype of an object, we use Object.getPrototypeOf
+            and Object.setPrototypeOf. Nowadays, in modern language, it is being
+            set using __proto__.
+          </p>
+        </div>
+      </div>
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="card-title">
+            Why you do not set the state directly in React. For example, if you
+            have const [products, setProducts] = useState([]). Why you do not
+            set products = [...] instead, you use the setProducts
+          </h2>
+          <p>
+            If we update it directly, calling the setState() afterward may just
+            replace the update you made. When we directly update the state, it
+            does not change this.state immediately. Instead, it creates a
+            pending state transition, and accessing it after calling this method
+            will only return the present value. we will lose control of the
+            state across all components.
+          </p>
+        </div>
+      </div>
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="card-title">
+            You have an array of products. Each product has a name, price,
+            description, etc. How will you implement a search to find products
+            by name?
+          </h2>
+          <p>
+            Firstly, we import useState from react . Then, we import the Scroll
+            and SearchList components. Next, in the Search function, we use the
+            useState hook to initialise the value of the searchField state with
+            useState("") (an empty string). After this, we use the filter
+            function on the details list received from the parent.
+          </p>
+        </div>
+      </div>
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="card-title">Card title!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Home/BusinessSummary/BusinessSummary.js
+++ src/Pages/Home/BusinessSummary/BusinessSummary.js
@@ -2,24 +2,70 @@ import React from "react";
 
 const BusinessSummary = () => {
   return (
-    <div>
-      <div class=" py-5 font-bold min-h-screen bg-base-200">
-        <div className="text-3xl text-center">
-          <h1>Business With Trust</h1>
+    <div className="text-center place-items-center py-10">
+      <h1 className="text-3xl font-bold py-3">Business With Trust</h1>
+      <div class="stats shadow">
+        <div class="stat place-items-center">
+          <div class="stat-figure text-secondary ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block w-8 h-8 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+          </div>
+          <div class="stat-title">All-time Orders</div>
+          <div class="stat-value">131K</div>
+          <div class="stat-desc">Since 2010 - Present</div>
         </div>
-        <div className="justify-items-center items-center justify-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 6a3 3 0 013-3h10a1 1 0 01.8 1.6L14.25 8l2.55 3.4A1 1 0 0116 13H6a1 1 0 00-1 1v3a1 1 0 11-2 0V6z"
-              clip-rule="evenodd"
-            />
-          </svg>
+
+        <div class="stat place-items-center">
+          <div class="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block w-8 h-8 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+              ></path>
+            </svg>
+          </div>
+          <div class="stat-title">New Users</div>
+          <div class="stat-value">4,200</div>
+          <div class="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div class="stat place-items-center">
+          <div class="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block w-8 h-8 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
+              ></path>
+            </svg>
+          </div>
+          <div class="stat-title">New Registers</div>
+          <div class="stat-value">1,200</div>
+          <div class="stat-desc">↘︎ 90 (14%)</div>
         </div>
       </div>
     </div>
